bf3cf9fd81b41fe0de30063613b0a441e78eb4fd
Scharage <scharage3@gmail.com>
2024-12-02 22:06:36 +0100

    6. Commit Fetching Dates works

--- backend/src/wichtel/wichtelService.ts
+++ backend/src/wichtel/wichtelService.ts
@@ -1,24 +1,28 @@
-import { WichtelData, WichteleeData, WichtelInfo } from "../models/models"
+import { WichtelData, WichteleeData } from "../models/models"
 import DataBaseInstance from "../services/databaseService"
 const config = require("../config.json")
 
 export async function setWichtelData(req: any) {
-  const userId: number = req.auth.userId
-  const wichtelData: WichtelData = req.body
-  await DataBaseInstance.setWichtelData(userId, wichtelData)
+    const userId: number = req.auth.userId
+    const wichtelData: WichtelData = req.body
+    await DataBaseInstance.setWichtelData(userId, wichtelData)
 }
 
 export async function getWichtelData(req: any): Promise<WichtelData> {
-  const userId: number = req.auth.userId
-  const timestamp: number = config.wichteln.enddate
-  const wichtelInfo: WichtelInfo = {
-    ...(await DataBaseInstance.getWichtelData(userId)),
-    date: timestamp,
-  }
-  return wichtelInfo
+    const userId: number = req.auth.userId
+    const wichtelInfo: WichtelData = {
+        ...(await DataBaseInstance.getWichtelData(userId)),
+    }
+    return wichtelInfo
 }
 
 export async function getWichtelee(req: any): Promise<WichteleeData> {
-  const userId: number = req.auth.userId
-  return await DataBaseInstance.getWichteleeData(userId)
+    const userId: number = req.auth.userId
+    return await DataBaseInstance.getWichteleeData(userId)
+}
+
+export async function startWichtel() {}
+
+export async function getWichtelDate(): Promise<String> {
+    return config.wichteln.enddate
 }
